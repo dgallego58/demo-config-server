f3fc52b161adafd0a6f20cbc19d0beeecd02fe21
daagalle <daagalle@bancolombia.com.co>
2021-05-12 17:30:45 -0500

    added remote

---
+++ src/test/java/co/com/properties/server/DemoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.properties.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
